a6331f0d2ef82b440b75ba8dc75dec03eef4378b
drdynscript <drdynscript@gmail.com>
2024-09-25 13:25:35 +0200

    Changed repo name

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -12,59 +12,16 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Technologie', link: '/technology' },
-      { text: 'Markup', link: '/markup' },
-      { text: 'Styling', link: '/styling' },
+      { text: 'Figma', link: '/figma' },
       { text: 'Richtlijnen', link: '/guidelines' },
     ],
 
     sidebar: {
-      '/technology/': [
+      '/figma/': [
         {
-          text: 'Technologie',
+          text: 'Figma',
           items: [
-            { text: 'Introductie', link: '/technology' },
-            { text: 'Netwerk', link: '/technology/network' },
-            { text: 'OSI', link: '/technology/osi' },
-            { text: 'Protocollen', link: '/technology/protocols' },
-            { text: 'Internet', link: '/technology/internet' },
-            { text: 'World Wide Web', link: '/technology/web' },
-            { text: 'Applicaties', link: '/technology/applications' },
-            { text: 'Source Code Management', link: '/technology/scm' },
-            { text: 'Command Line Interface', link: '/technology/cli' },
-          ]
-        }
-      ],
-      '/markup/': [
-        {
-          text: 'Markup',
-          items: [
-            { text: 'Introductie', link: '/markup' },
-            { text: 'Structuur', link: '/markup/structure' },
-            { text: 'Basis head-elementen', link: '/markup/basic-head-elements' },
-            { text: 'Tekst', link: '/markup/text' },
-            { text: 'Lijsten', link: '/markup/lists' },
-            { text: 'Linken', link: '/markup/links' },
-            { text: 'Afbeeldingen', link: '/markup/images' },
-            { text: 'Tabellen', link: '/markup/tables' },
-            { text: 'Formulieren', link: '/markup/forms' },
-            { text: 'Lay-out', link: '/markup/layout' },
-          ]
-        }
-      ],
-      '/styling/': [
-        {
-          text: 'Styling',
-          items: [
-            { text: 'Introductie', link: '/styling' },
-            { text: 'Selectoren', link: '/styling/selectors' },
-            { text: 'Waarden', link: '/styling/values' },
-            { text: 'Boxmodel', link: '/styling/box-model' },
-            { text: 'Typografie', link: '/styling/typography' },
-            { text: 'Kleursystemen', link: '/styling/color-systems' },
-            { text: 'Boxstijlen', link: '/styling/box-styling' },
-            { text: 'Lay-out', link: '/styling/layout' },
-            { text: 'Media Queries', link: '/styling/media-queries' },
+            { text: 'Introductie', link: '/figma' },
           ]
         }
       ],
@@ -73,11 +30,6 @@ export default defineConfig({
           text: 'Richtlijnen',
           items: [
             { text: 'Introductie', link: '/guidelines' },
-            { text: 'Semantiek', link: '/guidelines/semantics' },
-            { text: 'Coding', link: '/guidelines/coding' },
-            { text: 'Gebruiksvriendelijkheid', link: '/guidelines/usability' },
-            { text: 'Performantie', link: '/guidelines/performance' },
-            { text: 'Toegankelijkheid', link: '/guidelines/accessibility' },
           ]
         }
       ],
